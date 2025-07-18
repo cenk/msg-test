26b1f578af4d17d70e7780b2066bf2beab5e987a
cenk <git@cenk.app>
2025-07-18 22:43:53 +0300

    Update welcome_message.py

--- src/endstone_welcome_message/welcome_message.py
+++ src/endstone_welcome_message/welcome_message.py
@@ -10,13 +10,12 @@ class WelcomeMessage(Plugin):
         self.save_default_config()
         self.register_events(self)
         self.welcome_message_enabled = bool(self.config[welcome_message]["enabled"])
+        self.welcome_message_type = str(self.config[welcome_message]["type"])
+        self.welcome_message_title = str(self.config[welcome_message]["title"])
+        self.welcome_message_text = str(self.config[welcome_message]["text"])
+        self.welcome_message_wait_before = int(self.config[welcome_message]["wait_before"])
 
-        if self.welcome_message_enabled:
-            self.welcome_message_type = str(self.config[welcome_message]["type"])
-            self.welcome_message_title = str(self.config[welcome_message]["title"])
-            self.welcome_message_text = str(self.config[welcome_message]["text"])
-            self.welcome_message_wait_before = int(self.config[welcome_message]["wait_before"])
-        else
+        if not self.welcome_message_enabled:
             self.logger.info("Welcome Message is disabled in the config file!")
 
     @event_handler
