fa560645d4c139e69b5d9e56e9c90be5e2090612
cenk <git@cenk.app>
2025-07-18 03:09:22 +0300

    first commit

---
+++ src/endstone_welcome_message/welcome_message.py
@@ -0,0 +1,17 @@
+from endstone import ColorFormat
+from endstone.plugin import Plugin
+from endstone.event import event_handler, PlayerJoinEvent
+
+class WelcomeMessage(Plugin):
+    api_version = "0.9"
+
+    def on_load(self) -> None:
+        self.logger.info("on_load is called!")
+
+    def on_enable(self) -> None:
+        self.logger.info("on_enable is called!")
+        self.register_events(self)
+
+    @event_handler
+    def on_player_join(self, event: PlayerJoinEvent):
+        self.player.send_toast("Welcome", ColorFormat.YELLOW + f"Welcome {event.player.name}!")
