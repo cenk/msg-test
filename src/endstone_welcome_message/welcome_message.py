d7e14122f5e48a688c67417891d4965fa47498fa
cenk <git@cenk.app>
2025-07-18 22:19:16 +0300

    Update welcome_message.py

--- src/endstone_welcome_message/welcome_message.py
+++ src/endstone_welcome_message/welcome_message.py
@@ -9,21 +9,22 @@ class WelcomeMessage(Plugin):
     def on_enable(self) -> None:
         self.save_default_config()
         self.register_events(self)
-        self.welcome_message_type = self.config[welcome_message]["type"]
-        self.welcome_message_title = self.config[welcome_message]["title"]
-        self.welcome_message_text = self.config[welcome_message]["text"]
-        self.welcome_message_wait_before = self.config[welcome_message]["wait_before"]
+        self.welcome_message_enabled = bool(self.config[welcome_message]["enabled"])
+        self.welcome_message_type = str(self.config[welcome_message]["type"])
+        self.welcome_message_title = str(self.config[welcome_message]["title"])
+        self.welcome_message_text = str(self.config[welcome_message]["text"])
+        self.welcome_message_wait_before = int(self.config[welcome_message]["wait_before"])
         
     @event_handler
     def on_player_join(self, event: PlayerJoinEvent):
         match self.welcome_message_type:
             case 0:
                 self.logger.info("Welcome Message is disabled in the config file!")
-            case 1:
+            case "chat":
                 event.player.send_message(self.welcome_message_text)
-            case 2:
+            case "tip":
                 event.player.send_tip(self.welcome_message_text)
-            case 3:
+            case "toast":
                 event.player.send_toast(self.welcome_message_title, self.welcome_message_text)
             case _:
                 pass
