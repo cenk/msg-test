7c824e95686a9db635b37f59dee72b049d3d65fb
cenk <git@cenk.app>
2025-07-18 19:57:14 +0300

    Update welcome_message.py

--- src/endstone_welcome_message/welcome_message.py
+++ src/endstone_welcome_message/welcome_message.py
@@ -9,10 +9,10 @@ class WelcomeMessage(Plugin):
     def on_enable(self) -> None:
         self.save_default_config()
         self.register_events(self)
-        self.welcome_message_type = self.config[welcome_message]["type"]
-        self.welcome_message_title = self.config[welcome_message]["title"]
-        self.welcome_message_text = self.config[welcome_message]["text"]
-        self.welcome_message_wait_before = self.config[welcome_message]["wait_before"] 
+        self.welcome_message_type: int = self.config[welcome_message]["type"]
+        self.welcome_message_title: str = self.config[welcome_message]["title"]
+        self.welcome_message_text: str = self.config[welcome_message]["text"]
+        self.welcome_message_wait_before: int = self.config[welcome_message]["wait_before"] 
         
     @event_handler
     def on_player_join(self, event: PlayerJoinEvent):
