dada9c1556b731e59e84c894644a5ea07128ea1e
cenk <git@cenk.app>
2025-07-19 19:41:18 +0300

    Update welcome_message.py

--- src/endstone_welcome_message/welcome_message.py
+++ src/endstone_welcome_message/welcome_message.py
@@ -13,8 +13,8 @@ class WelcomeMessage(Plugin):
         self.welcome_message_type = max(0, min(int(self.config["welcome_message"]["type"]), 6))
 
         if self.welcome_message_type > 0:
-            self.welcome_message_header = f'{str(self.config["welcome_message"]["header"])}'
-            self.welcome_message_body = f'{str(self.config["welcome_message"]["body"])}'
+            self.welcome_message_header = str(self.config["welcome_message"]["header"])
+            self.welcome_message_body = str(self.config["welcome_message"]["body"])
             self.welcome_message_wait_before = max(0, min(int(self.config["welcome_message"]["wait_before"]), 10))
         else:
             self.logger.info("Welcome Message is disabled in the config file!")
@@ -32,7 +32,7 @@ class WelcomeMessage(Plugin):
                 case 3:
                     event.player.send_popup(self.welcome_message_body)
                 case 4:
-                    event.player.send_toast(self.welcome_message_header, self.welcome_message_body)
+                    event.player.send_toast(f'{self.welcome_message_header}', f'{self.welcome_message_body)}'
                 case 5:
                     event.player.send_title(self.welcome_message_header, self.welcome_message_body)
                 case 6:
